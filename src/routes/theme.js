8c91a78d51be9f8471e2fd82b3c3ddc0d24f95a3
tovade <66245404+tovade@users.noreply.github.com>
2020-09-25 19:16:02 +0200

    Aspect bot stuff
    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- src/routes/theme.js
+++ src/routes/theme.js
@@ -8,8 +8,6 @@ route.get("/", async (req, res) => {
     res.set('Cache-Control', 'no-store');
     let theme = req.cookies["theme"]
     if (!theme) return res.sendFile(path.join(__dirname, "../dynamic/theme/light.css"))
-    else if (theme === "dark") return res.sendFile(path.join(__dirname, "../dynamic/theme/dark.css"));
-    else return res.sendFile(path.join(__dirname, "..//dynamic/theme/light.css"))
 });
 
 module.exports = route;
